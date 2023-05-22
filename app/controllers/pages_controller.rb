b3187deb5d559d2d6d42e54e54a35696cd99630f
Uthaeus <romanlavery@gmail.com>
2023-05-21 22:11:18 -0600

    working on adding heart button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, except: %i[ home ]
+
   def home
   end
 
   def options
+  end
+
+  def user_info
+    @users = User.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :posts
   root 'pages#home'
   get 'options', to: 'pages#options'
+  get 'user_info', to: 'pages#user_info'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
