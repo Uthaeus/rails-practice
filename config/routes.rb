93959ae62bbd6338c7746437130cae3f15ebc416
Uthaeus <romanlavery@gmail.com>
2023-05-21 20:46:01 -0600

    generated controller for pages, scaffold for posts, installed sassc

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
+  get 'home', to: 'pages#home'
+  get 'options', to: 'pages#options'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
